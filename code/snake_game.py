ac413eba5d6cd63a9c0bcd066d956278369a1e90
AbhishekMankame <abhishekmankame962@gmail.com>
2023-06-03 15:34:38 +0530

    simple_update

--- code/snake_game.py
+++ code/snake_game.py
@@ -56,6 +56,7 @@ class SnakeGameAI:
         self.score = 0
         self.food = None
         self._place_food()
+        self.frame_iteration = 0
         
     def _place_food(self):
         x = random.randint(0, (self.w-BLOCK_SIZE )//BLOCK_SIZE )*BLOCK_SIZE 
