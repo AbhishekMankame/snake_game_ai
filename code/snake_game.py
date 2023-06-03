b6a984a8fb33b9e5fedd110579c3fa9f95e462d8
AbhishekMankame <abhishekmankame962@gmail.com>
2023-06-03 21:37:56 +0530

    simple_update

--- code/snake_game.py
+++ code/snake_game.py
@@ -136,6 +136,7 @@ class SnakeGameAI:
         elif np.array_equal(action, [0,1,0]):
             next_idx = (idx+1)%4
             new_dir = clock_wise[next_idx] # right turn r -> d -> l -> u
+        else: # [0,0,1]
 
         x = self.head.x
         y = self.head.y
