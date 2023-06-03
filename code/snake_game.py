3ba5974770d16187800078fdf1b30c3d1a2a03dd
AbhishekMankame <abhishekmankame962@gmail.com>
2023-06-03 15:36:52 +0530

    simple_update

--- code/snake_game.py
+++ code/snake_game.py
@@ -65,21 +65,13 @@ class SnakeGameAI:
         if self.food in self.snake:
             self._place_food()
         
-    def play_step(self):
+    def play_step(self, action):
         # 1. collect user input
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    self.direction = Direction.LEFT
-                elif event.key == pygame.K_RIGHT:
-                    self.direction = Direction.RIGHT
-                elif event.key == pygame.K_UP:
-                    self.direction = Direction.UP
-                elif event.key == pygame.K_DOWN:
-                    self.direction = Direction.DOWN
+
         
         # 2. move
         self._move(self.direction) # update the head
@@ -127,7 +119,7 @@ class SnakeGameAI:
         self.display.blit(text, [0, 0])
         pygame.display.flip()
         
-    def _move(self, direction):
+    def _move(self, action):
         x = self.head.x
         y = self.head.y
         if direction == Direction.RIGHT:
