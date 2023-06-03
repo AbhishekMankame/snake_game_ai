0545f1b64e0331321a50563f47e0b3f11a460536
AbhishekMankame <abhishekmankame962@gmail.com>
2023-06-03 15:42:43 +0530

    simple_lenght_update

--- code/snake_game.py
+++ code/snake_game.py
@@ -80,9 +80,10 @@ class SnakeGameAI:
         # 3. check if game over
         reward = 0
         game_over = False
-        if self._is_collision():
+        if self._is_collision() or self.frame_iteration > 100*len(self.snake):
             game_over = True
-            return game_over, self.score
+            reward = -10
+            return reward, game_over, self.score
             
         # 4. place new food or just move
         if self.head == self.food:
