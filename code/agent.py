620ed4f9aa97e36fbe17fb41d45d5fd779734cad
AbhishekMankame <abhishekmankame962@gmail.com>
2023-06-04 22:44:44 +0530

    simple_update

--- code/agent.py
+++ code/agent.py
@@ -92,6 +92,8 @@ class Agent:
         # random moves: tradeoff exploration / exploitation
         self.epsilon = 80 - self.n_games
         final_move = [0,0,0]
+        if random.randint(0,200) < self.epsilon:
+            move = random.randint(0, 2)
 
 def train():
     plot_scores = []
