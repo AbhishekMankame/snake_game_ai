7eec56fe65f61428d2879a7a5e3802bdefde8390
AbhishekMankame <abhishekmankame962@gmail.com>
2023-06-04 23:06:12 +0530

    folder_creation_update

--- code/model.py
+++ code/model.py
@@ -17,3 +17,5 @@ class Linear_QNet(nn.Module):
     
     def save(self, file_name='model.pth'):
         model_folder_path = './model'
+        if not os.path.exists(model_folder_path):
+            os.makedirs(model_folder_path)
