398213e013df8492fe4538450b1ee2dc8200ed55
AbhishekMankame <abhishekmankame962@gmail.com>
2023-06-04 23:22:31 +0530

    function_import

--- code/agent.py
+++ code/agent.py
@@ -3,6 +3,7 @@ import random
 import numpy as np
 from collections import deque
 from game import SnakeGameAI, Direction, Point
+from model import Linear_QNet, QTrainer
 
 MAX_MEMORY = 100_000
 BATCH_SIZE = 1000
