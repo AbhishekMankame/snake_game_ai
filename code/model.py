75c7c46c625de8ee00993ecf8988c91dce341e3e
AbhishekMankame <abhishekmankame962@gmail.com>
2023-06-04 23:03:39 +0530

    helper_function_update

--- code/model.py
+++ code/model.py
@@ -10,7 +10,9 @@ class Linear_QNet(nn.Module):
         self.linear1 = nn.Linear(input_size, hidden_size)
         self.linear2 = nn.Linear(hidden_size, output_size)
 
-    def forward(self, x):
+    def forward(self, x):           #forward function
         x = F.relu(self.linear1(x))
         x = self.linear2(x)
         return x
+    
+    def save(self, file_name='model.pth')
