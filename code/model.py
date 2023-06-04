81924ea17782c664a0fa77e2b04df6f0d195f25e
AbhishekMankame <abhishekmankame962@gmail.com>
2023-06-04 22:59:41 +0530

    relu1_update

--- code/model.py
+++ code/model.py
@@ -9,3 +9,6 @@ class Linear_QNet(nn.Module):
         super().__init()
         self.linear1 = nn.Linear(input_size, hidden_size)
         self.linear2 = nn.Linear(hidden_size, output_size)
+
+    def forward(self, x):
+        x = F.relu(self.linear1(x))
