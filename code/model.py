0ab91cc5cb90369b5a01ec1971c9fab71edcee8b
AbhishekMankame <abhishekmankame962@gmail.com>
2023-06-05 11:38:01 +0530

    simple_update

--- code/model.py
+++ code/model.py
@@ -6,7 +6,7 @@ import os
 
 class Linear_QNet(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
-        super().__init()
+        super().__init()__
         self.linear1 = nn.Linear(input_size, hidden_size)
         self.linear2 = nn.Linear(hidden_size, output_size)
 
